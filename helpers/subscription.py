cdef20818ed8b0c25aaff28f6054f7ab79a40504
Amlan <amlannandy5@gmail.com>
2021-03-03 10:24:05 +0530

    Added unsubcribe form

--- helpers/subscription.py
+++ helpers/subscription.py
@@ -7,6 +7,11 @@ from models.Subscriber import Subscriber
 def find_by_email(email):
   return Subscriber.query.filter_by(email=email).first()
 
+def delete_by_email(email):
+  subscriber = Subscriber.query.filter_by(email=email).first()
+  db.session.delete(subscriber)
+  db.session.commit()
+
 def save(subscriber):
   db.session.add(subscriber)
   db.session.commit()
--- routes/subscription.py
+++ routes/subscription.py
@@ -7,7 +7,7 @@ from flask import Blueprint, request, render_template
 from wtforms import Form, TextField, validators
 
 from models.Subscriber import Subscriber
-from helpers.subscription import find_by_email, save
+from helpers.subscription import find_by_email, save, delete_by_email
 
 subscription = Blueprint('subscription', __name__)
 
@@ -17,7 +17,7 @@ class SubscriptionForm(Form):
 
   # Open index page of website
   @subscription.route('/', methods=['GET', 'POST'])
-  def open_home_page():
+  def open_subscribe_page():
     form = SubscriptionForm(request.form)
 
     if request.method == 'POST':
@@ -34,5 +34,19 @@ class SubscriptionForm(Form):
         save(subscriber)
         flash('Success! You have now subscribed to the mailing list', 'success')
     
-    return render_template('index.html', form=form)
+    return render_template('subscribe.html', form=form)
+
+@subscription.route('/unsubscribe/<email>', methods=['GET', 'POST'])
+def open_unsubscribe_page(email):
+
+  if request.method == 'POST':
+
+    # Check if a subsriber with that email exists
+    subscriber = find_by_email(email)
+    if not subscriber:
+      flash('Oops! Looks like you have already unsubscribed', 'warning')
+    else:
+      delete_by_email(email)
+      flash('Success! You will no longer get these emails', 'success')
   
+  return render_template('unsubscribe.html')  
