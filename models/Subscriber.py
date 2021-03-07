b2b4a46ea7f19a5160091973024f43710cb1465c
Amlan <amlannandy5@gmail.com>
2021-03-07 22:06:56 +0530

    possible fix

--- models/Subscriber.py
+++ models/Subscriber.py
@@ -7,8 +7,8 @@ from app import db
 
 class Subscriber(db.Model):
   id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(100), nullable=False)
-  email = db.Column(db.String(120), unique=True, nullable=False)
+  name = db.Column(db.String, nullable=False)
+  email = db.Column(db.String, unique=True, nullable=False)
   created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
   def __init__(self, name, email):
