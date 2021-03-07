2b2928825f83cf786c33d92519b86a72e1ef6bc2
Amlan <amlannandy5@gmail.com>
2021-03-07 21:55:45 +0530

    possible fix

--- models/Article.py
+++ models/Article.py
@@ -9,7 +9,7 @@ class Article(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   title = db.Column(db.String(200), nullable=False, unique=True)
   content = db.Column(db.String(2000), nullable=False)
-  url = db.Column(db.String(120), nullable=False)
+  url = db.Column(db.String(300), nullable=False)
   created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
   def __init__(self, title, content, url):
