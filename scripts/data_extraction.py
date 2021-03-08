b74979bc8fbfe93b24efcfd05f862f207992b250
Amlan <amlannandy5@gmail.com>
2021-03-08 12:38:34 +0530

    Added timestamping to script

--- scripts/data_extraction.py
+++ scripts/data_extraction.py
@@ -4,7 +4,7 @@ sys.path.append('..')
 from app import app
 from scripts.scrapper import extract_articles
 from helpers.send_email import send_article_email
-from helpers.admin import save_article_from_json, find_by_title, fetch_all_user_emails
+from helpers.admin import save_article_from_json, find_by_title, fetch_all_user_emails, add_timestamp
 
 def extract_data_from_source():
   with app.app_context():
@@ -17,5 +17,6 @@ def extract_data_from_source():
         count += 1
         save_article_from_json(article)
         send_article_email(user_emails, article)
+    add_timestamp(count)
     print(f'{count} new articles scrapped')
     return count
